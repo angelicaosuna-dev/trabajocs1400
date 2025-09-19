7bd21fcc007d94eef6b0b019c64588a7f33dc5dc
angelicaosuna-dev <angelicaosuna@mail.weber.edu>
2025-09-18 19:14:24 -0600

    agregando arcivo de saludo

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+nombre = input("hola como te llamas? ")
+edad = int(input("cuantos años tienes? "))
+if edad < 18:
+    print("eres menor de edad")
+elif edad >18:
+    print("eres mayor de edad")
+else:
+    print("hola")
